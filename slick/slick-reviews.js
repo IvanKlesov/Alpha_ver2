b68bb889292a4a7cf24c3fa10cb0f3050ce3a62b
Ivan Klesov <jonhbird@rambler.ru>
2019-02-04 21:49:14 +0300

    commit

--- slick/slick-reviews.js
+++ slick/slick-reviews.js
@@ -5,9 +5,8 @@ $(document).ready(function(){
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    dotsClass: "dots-review",
-    customPaging : function(reviews, i) {
-        return '<a href="#"><img src="./images/human1.jpg" /><img src="./images/human2.jpg" /><img src="./images/human3.jpg" /></a>';
-    },
-  });
+    prevArrow: null,
+    nextArrow: null,
+    dotsClass: "dots-review"
+    });
 });
