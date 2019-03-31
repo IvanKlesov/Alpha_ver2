46c71061cb8b91879b766f3c79c38d4b2c3ee66a
Ivan Klesov <jonhbird@rambler.ru>
2019-03-31 15:39:14 +0300

    work on gallery

---
+++ slick/slick-gallery.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.gallery').slick({
+    autoplay:true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    arrows: true,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    ],
+    swipe: true
+  });
+});
